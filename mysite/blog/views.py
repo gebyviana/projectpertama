6054dfd3120af764965e454348db2175fad2926d
gebyviana <gebyviana@gmail.com>
2017-02-13 15:09:33 +0700

    perubahan template

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
@@ -23,10 +23,10 @@ def post_new(request):
 			post.author = request.user
 			post.published_date = timezone.now()
 			post.save()
-			return redirect('post_detail', pk=post.pk)
+			return render(request, 'blog/post_detail.html', {'post': post})
 	else:
 		form = PostForm()
-	return render(request, 'blog/post_edit.html', {'form': form})
+	return render(request, 'blog/post_new.html', {'form': form})
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -37,7 +37,25 @@ def post_edit(request, pk):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail', pk=post.pk)
+            return render(request, 'blog/post_detail.html', {'post': post})
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def appetizer(request):
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'new/appetizer.html', {'posts': posts})
+	
+def maincourse(request):
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'new/maincourse.html', {'posts': posts})
+	
+def dessert(request):
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'new/dessert.html', {'posts': posts})
+	
+def hapus(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	categories = post.categories
+	post.delete()
+	return redirect('blog:'+categories)
